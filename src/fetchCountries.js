fd127c155665bb4e0ef5b42d17e60faa020452b3
Serhii <sergio.fly4405@gmail.com>
2023-01-14 17:57:53 +0200

    f1

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,3 +1,5 @@
+import { list } from "./inde";
+
 export function fetchCountries(name) {
     const URL_NAME = `https://restcountries.com/v3.1/name/${name}?capital,population,languages,flags.svg,name.official`;
     const filtr = {
@@ -5,7 +7,9 @@ export function fetchCountries(name) {
      }
      
   return  fetch(URL_NAME, filtr).then(resp => {
-
+    if (!resp.ok) {
+        throw new Error(response.status);
+      }
           return resp.json()
     })
 }
--- src/inde.js
+++ src/inde.js
@@ -1,39 +1,55 @@
 import _ from 'lodash';
 import './css/styles.css';
 import { fetchCountries } from './fetchCountries';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
 const inputValue = document.querySelector("#search-box")
+export const list = document.querySelector(".country-list")
 const dataInput = {
     nameCity: ''
 };
 
 inputValue.addEventListener("input", _.debounce(valueData, DEBOUNCE_DELAY, {leading: false}))
 
-
 function valueData(e) {
 
     dataInput.nameCity = e.target.value
     const { nameCity } = dataInput;
- 
 
-    if (nameCity.length > 2) {
+    fetchCountries(nameCity.trim()).then(nameCity => 
 
-        fetchCountries(nameCity).then(nameCity => {onFilter(nameCity[0])})
-                 
-    } else {
-        console.log("low simbols");
-    }
+       { 
+        
+        if(nameCity.length === 1) {
+       
+        const {capital,name,languages,population,flags} = nameCity[0]
+        const keyLanguage = Object.values(languages)
 
-    function onFilter(nameCity) {
-        const keys = Object.keys(nameCity.languages)
-        console.log(nameCity.name.common)
-        console.log(nameCity.capital[0])
-        console.log(nameCity.population)
-        for (const key of keys) {
-            console.log(nameCity.languages[key])
-        }
-    }
+        const showCity =  `<li style = "list-style: none"><h2 style="display:flex;/* margin: 0px; */align-items: center;"><img style = "margin: 10px" 
+        src="${flags.png}" alt="${name.common}" width = 50px> ${name.official}</h2>
+        <h3>Capital: <span style = "font-weight: 400">${capital}</span> </h3>
+        <h3>Population: <span style = "font-weight: 400">${population}</span>
+        </h3><h3>Language: <span style = "font-weight: 400">${keyLanguage}</span></h3></li>`
+        list.innerHTML =''
+        list.innerHTML += showCity
+        
+
+       } else if (nameCity.length < 10 && nameCity.length >= 2) {
+        
+        list.innerHTML =''
+        for (const city of nameCity) {
+            const {name, flags} = city
+            const showCitys = `<h2 style="font-weight: 400;font-size: 15px;"><img src="${flags.png}" alt="${name.common}" width = 30px style = "margin-right: 10px">${name.official}</h2>`
+           
+         list.innerHTML += showCitys
+
+        } 
+        } else {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.") 
+        } })
+        .catch(error => {Notiflix.Notify.failure("Oops, there is no country with that name");
+    list.innerHTML=''})
 } 
 
 
